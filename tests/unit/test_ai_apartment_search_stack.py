effe3d1aabead647254ce8dd5815ebff60c7b8da
Matthew Kosko <matthewdkosko@gmail.com>
2025-02-25 19:59:31 -0500

    Resolved merge conflicts

---
+++ tests/unit/test_ai_apartment_search_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from ai_apartment_search.ai_apartment_search_stack import AiApartmentSearchStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in ai_apartment_search/ai_apartment_search_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = AiApartmentSearchStack(app, "ai-apartment-search")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })

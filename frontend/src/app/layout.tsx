1c85c9cfa709ae73eb5353e84e8bd25a9ff032e9
Matthew Kosko <matthewdkosko@gmail.com>
2025-02-28 12:49:47 -0500

    Added skeleton frontend component, started full rental stack

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "NYC Apartment Search",
+  description: "Find the best apartments in NYC based on crime rate, parks, and more.",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

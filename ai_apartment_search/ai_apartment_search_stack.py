959f618171774875326f1b5b6315be3d8dcee69f
Matthew Kosko <matthewdkosko@gmail.com>
2025-02-28 12:53:30 -0500

    Updated gitignore

--- ai_apartment_search/ai_apartment_search_stack.py
+++ ai_apartment_search/ai_apartment_search_stack.py
@@ -1,19 +1,31 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
 from constructs import Construct
 
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_apigateway as apigw,
+    aws_route53 as route53,
+    aws_route53_targets as targets,
+    aws_certificatemanager as acm,
+    aws_lambda as _lambda,
+    aws_secretsmanager as secretsmanager,
+    Stack
+)
+
 class AiApartmentSearchStack(Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+        domain_name = "aiapartmentsnyc.com"
+        hosted_zone = route53.HostedZone.from_lookup(self, "HostedZone", domain_name=domain_name)
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AiApartmentSearchQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+app = core.App()
+AiApartmentSearchStack(app, "RentalAppStack")
+app.synth()
+
+

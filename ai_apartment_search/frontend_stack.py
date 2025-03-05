a84a737ea6d0071a1131e63732279048a397a1f7
Matthew Kosko <matthewdkosko@gmail.com>
2025-03-05 14:43:02 -0500

    Updated CDN distribution to add default root object

--- ai_apartment_search/frontend_stack.py
+++ ai_apartment_search/frontend_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_s3_deployment as s3_deployment,
     aws_cloudfront as cloudfront,
+    aws_iam as iam,
     aws_cloudfront_origins as origins
 )
 from constructs import Construct
@@ -25,11 +26,22 @@ class FrontendStack(Stack):
 
         distribution =cloudfront.Distribution(
             self, 'FrontendDistribution',
+            default_root_object="index.html",
             default_behavior=cloudfront.BehaviorOptions(
                 origin=origins.S3BucketOrigin(frontend_bucket),
                 viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
             )
         )
+        # distribution = cloudfront.Distribution(
+        #     self, "FrontendDistribution",
+        #     default_behavior=cloudfront.BehaviorOptions(
+        #         origin=origins.S3Origin(
+        #             frontend_bucket,  # FIX: Correct way to define S3 origin
+        #             origin_access_identity=None  # FIX: Required for OAC to work
+        #         ),
+        #         viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+        #     )
+        # )
 
         s3_deployment.BucketDeployment(
             self, 'FrontendDeployment',
@@ -37,6 +49,32 @@ class FrontendStack(Stack):
             sources=[s3_deployment.Source.asset("frontend/build")],
             retain_on_delete=False
         )
+
+        frontend_bucket.add_to_resource_policy(
+            iam.PolicyStatement(
+                actions=["s3:GetObject"],
+                resources=[frontend_bucket.arn_for_objects("*")],
+                principals=[iam.ServicePrincipal("cloudfront.amazonaws.com")],
+                conditions={
+                    "StringEquals": {
+                        "AWS:SourceArn": f"arn:aws:cloudfront::{cdk.Aws.ACCOUNT_ID}:distribution/{distribution.distribution_id}"
+                    }
+                }
+            )
+        )
+
+        oac = cloudfront.CfnOriginAccessControl(
+            self, "FrontendOAC",
+            origin_access_control_config=cloudfront.CfnOriginAccessControl.OriginAccessControlConfigProperty(
+            name="FrontendOAC",
+            origin_access_control_origin_type="s3",
+            signing_behavior="always", 
+            signing_protocol="sigv4"  
+        )
+    )
+        
+        cfn_distribution = distribution.node.default_child
+        cfn_distribution.add_property_override("DistributionConfig.Origins.0.OriginAccessControlId", oac.ref)
 
         cdk.CfnOutput(
             self, "CloudFrontURL",

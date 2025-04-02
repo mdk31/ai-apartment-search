95b2f0af9c35e75dfefdc846eebefedfaa36f67f
Matthew Kosko <matthewdkosko@gmail.com>
2025-04-01 21:21:56 -0400

    Added VPC code to network stack

--- ai_apartment_search/networking_stack.py
+++ ai_apartment_search/networking_stack.py
@@ -13,7 +13,7 @@ class DatabaseStack(Stack):
         super().__init__(scope, id, **kwargs)
     
         vpc = ec2.Vpc(
-            self, "DatabaseVPC",
+            self, "AppVPC",
             nat_gateways=1,
             subnet_configuration=[
                 ec2.SubnetConfiguration(
@@ -28,55 +28,68 @@ class DatabaseStack(Stack):
         )
 
 
-        db_security_group = ec2.SecurityGroup(
-            self, "DBSecurityGroup",
-            vpc=vpc,
-            description="The security group for the PostgreSQL database"
-        )
-
-        lambda_security_group = ec2.SecurityGroup(
-            self, "LambdaSecurityGroup",
-            vpc=vpc,
-            description="The security group for lambda functions to connect to the database"
-        )
-
-
-
-        # db_secret_policy = iam.PolicyStatement(
-        #     actions=["secretsmanager:GetSecretValue"],
-        #     resources=[db_secret.secret_arn],
-        #     principals=[iam.ServicePrincipal("lambda.amazonaws.com")]
+        # db_security_group = ec2.SecurityGroup(
+        #     self, "DBSecurityGroup",
+        #     vpc=vpc,
+        #     description="The security group for the PostgreSQL database"
         # )
 
-        # allow lambda function to get db secrets
-        # db_secret.add_to_resource_policy(db_secret_policy)
+        # lambda_security_group = ec2.SecurityGroup(
+        #     self, "LambdaSecurityGroup",
+        #     vpc=vpc,
+        #     description="The security group for lambda functions to connect to the database"
+        # )
 
-        database = rds.DatabaseInstance(
-            self, "PostgreSQLInstance",
-            engine=rds.DatabaseInstanceEngine.postgres(version=rds.PostgresEngineVersion.VER_13),
-            vpc=vpc,
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
-            allocated_storage='20GB',
-            max_allocated_storage='100GB',
-            storage_type=rds.StorageType.GP2,
-            credentials=rds.Credentials.from_secret(db_secret),
-            security_groups=[db_security_group],
-            publicly_accessible=False, # default
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
-            backup_retention=cdk.Duration(days=7),
-            delete_automated_backups=True,
-            removal_policy=cdk.RemovalPolicy.RETAIN
-        )
+        # db_security_group.add_ingress_rule(
+        #     peer=lambda_security_group,
+        #     connection=ec2.Port.POSTGRES
+        # )
 
-        cdk.CfnOutput(
-            self, 'VPCID',
-            value=vpc.vpc_id,
-            description='The VPC ID where the DB is deployed'
-        )
+        # db_secret = secretsmanager.Secret(
+        #     self, 'DBSSecret',
+        #     secret_name='PostgreSQLCredentials',
+        #     generate_secret_string=secretsmanager.SecretStringGenerator(
+        #         secret_string_template='{"username": "postgres"}',
+        #         generate_string_key='password',
+        #         exclude_characters='"@/\\'
+        #     )
+        # )
 
-        cdk.CfnOutput(
-            self, 'LambdaSecurityGroupID',
-            value=lambda_security_group.security_group_id,
-            description="The security group ID for lamda functions that need DB access")
+        # # db_secret_policy = iam.PolicyStatement(
+        # #     actions=["secretsmanager:GetSecretValue"],
+        # #     resources=[db_secret.secret_arn],
+        # #     principals=[iam.ServicePrincipal("lambda.amazonaws.com")]
+        # # )
+
+        # # allow lambda function to get db secrets
+        # # db_secret.add_to_resource_policy(db_secret_policy)
+
+        # database = rds.DatabaseInstance(
+        #     self, "PostgreSQLInstance",
+        #     engine=rds.DatabaseInstanceEngine.postgres(version=rds.PostgresEngineVersion.VER_13),
+        #     vpc=vpc,
+        #     instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+        #     allocated_storage='20GB',
+        #     max_allocated_storage='100GB',
+        #     storage_type=rds.StorageType.GP2,
+        #     credentials=rds.Credentials.from_secret(db_secret),
+        #     security_groups=[db_security_group],
+        #     publicly_accessible=False, # default
+        #     vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
+        #     backup_retention=cdk.Duration(days=7),
+        #     delete_automated_backups=True,
+        #     removal_policy=cdk.RemovalPolicy.RETAIN
+        # )
+
+        # cdk.CfnOutput(
+        #     self, 'VPCID',
+        #     value=vpc.vpc_id,
+        #     description='The VPC ID where the DB is deployed'
+        # )
+
+        # cdk.CfnOutput(
+        #     self, 'LambdaSecurityGroupID',
+        #     value=lambda_security_group.security_group_id,
+        #     description="The security group ID for lamda functions that need DB access")
         
 

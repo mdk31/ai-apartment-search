46373a798b8656361f48d937fb979d0738239531
Matthew Kosko <matthewdkosko@gmail.com>
2025-04-14 19:58:35 -0400

    Added db secrest to fetch active rental lambda

--- lambda_files/fetch_active_rentals_lambda/handler.py
+++ lambda_files/fetch_active_rentals_lambda/handler.py
@@ -2,20 +2,25 @@ import json
 import os
 import boto3
 import boto3
+import psycopg2
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 
 # TODO: Add tests
 # TODO: integrate get_openai with the rest of the function
 
 sm_client = boto3.client('secretsmanager')
-cache_config = SecretCacheConfig()
-cache = SecretCache(config=cache_config, client=sfn_client)
+SECRET_NAME = os.environ["DBSECRET"]
+cache = SecretCache(config=SecretCacheConfig(), client=sm_client)
 
 
-def get_openai_key():
-    secret_name = os.environ["OPENAI_SECRET_NAME"]
-    secret_value = cache.get_secret_string(secret_name)
-    return json.loads(secret_value)["api_key"]
+def get_db_secret():
+    secret_value = cache.get_secret_string(SECRET_NAME)
+    return json.loads(secret_value)
+
+def connect_to_db(creds):
+    return psycopg2.connect(
+        
+    )
 
 def lambda_handler(event, context):
     try:

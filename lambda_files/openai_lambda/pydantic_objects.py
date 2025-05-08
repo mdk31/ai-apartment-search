9d2ab41b445e16bf696d4837c5b69d5306df51d6
Matthew Kosko <matthewdkosko@gmail.com>
2025-05-07 20:41:15 -0400

    Started prompts for LLM

--- lambda_files/openai_lambda/pydantic_objects.py
+++ lambda_files/openai_lambda/pydantic_objects.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, Field
+from typing import List, Union
 
+class JoinCondition(BaseModel):
+    name: str 
+    args: List[Union[str, int, float]]
+
+class Join(BaseModel)
 
 class SqlOutput(BaseModel):
     """Safe SQL output"""

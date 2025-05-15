559c7586c779b2898aa473bc223be8f00ff7b5f2
Matthew Kosko <matthewdkosko@gmail.com>
2025-05-14 21:13:59 -0400

    Added networking stack to app

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 
 import aws_cdk as cdk
 from ai_apartment_search.waf_stack import WafStack
+from ai_apartment_search.networking_stack import NetworkingStack
 
 
 app = cdk.App()
 WafStack(app, 'WafStack')
+NetworkingStack(app, 'NetworkingStack')
+
 
 # FrontendStack(app, "FrontendStack")
 
